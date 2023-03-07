6dea3a145dbb3bd276c1acf1be14307755ddcc39
tdtnh8321 <92788899+tdtnh8321@users.noreply.github.com>
2023-03-07 10:16:08 +0700

    6

--- src/index.js
+++ src/index.js
@@ -34,4 +34,6 @@ router(app);
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log("Server is running on port", PORT);
+  console.log("Customer ", process.env.CLIENT_CUSTOMER_URL);
+  console.log("ADmin ", process.env.CLIENT_ADMIN_URL);
 });
